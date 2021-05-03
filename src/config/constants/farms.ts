bac0eb7a1f2537255e8054e60147e0dd82f15610
netfily <83597725+netfily@users.noreply.github.com>
2021-05-04 01:05:10 +0700

    Update farms.ts

--- src/config/constants/farms.ts
+++ src/config/constants/farms.ts
@@ -4,12 +4,12 @@ import { FarmConfig, QuoteToken } from './types'
 const farms: FarmConfig[] = [
   {
     pid: 0,
-    lpSymbol: 'CAKE',
+    lpSymbol: 'BACKUP',
     lpAddresses: {
       97: '0x9C21123D94b93361a29B2C2EFB3d5CD8B17e0A9e',
       56: '0x48e15ABBcb21C7f564c1B5Ae08A9f7829745F6D7',
     },
-    tokenSymbol: 'SYRUP',
+    tokenSymbol: 'SUNI',
     tokenAddresses: {
       97: '0xe9e7cea3dedca5984780bafc599bd69add087d56',
       56: '0x48e15ABBcb21C7f564c1B5Ae08A9f7829745F6D7',
@@ -19,12 +19,12 @@ const farms: FarmConfig[] = [
   },
   {
     pid: 1,
-    lpSymbol: 'CAKE-BNB LP',
+    lpSymbol: 'BACKUP-BNB LP',
     lpAddresses: {
       97: '0xe70b7523f4bffa1f2e88d2ba709afd026030f412',
       56: '0x2a1e4CdDf51Ab15A90EDeb56C7d132ef7915cCa9',
     },
-    tokenSymbol: 'CAKE',
+    tokenSymbol: 'BACKUP',
     tokenAddresses: {
       97: '0xe9e7cea3dedca5984780bafc599bd69add087d56',
       56: '0x48e15ABBcb21C7f564c1B5Ae08A9f7829745F6D7',
