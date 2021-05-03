6f316a1c245a15545f5475b061f5396e7bf8b72b
netfily <83597725+netfily@users.noreply.github.com>
2021-05-04 01:56:36 +0700

    Update contracts.ts

--- src/config/constants/contracts.ts
+++ src/config/constants/contracts.ts
@@ -28,7 +28,7 @@ export default {
     56: '0x5e74094Cd416f55179DBd0E45b1a8ED030e396A1',
   },
   mulltiCall: {
-    56: '0x1ee38d535d541c55c9dae27b12edf090c608e6fb',
+    56: '0xD72b7DdC16fddea782605875DdC7Fc5147F33be3',
     97: '0x67ADCB4dF3931b0C5Da724058ADC2174a9844412',
   },
   busd: {
